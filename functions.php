3fa222e9236e8351d0e6b9bdcf24ec2ce0d4d7ff
mirkobechini <mirkobechini@gmail.com>
2025-12-04 20:56:39 +0100

    Add addAlbum function to handle album additions in functions.php

--- functions.php
+++ functions.php
@@ -4,4 +4,12 @@ function readAlbums(string $filePath){
     $albumFile = file_get_contents($filePath);
     return $albumList = json_decode($albumFile, true);
 }
+
+
+function addAlbum(string $filePath, array $newAlbum){
+    $albumList = json_decode(file_get_contents($filePath), true);
+    $albumList[] = $newAlbum;
+    $json_album = json_encode($albumList);
+    file_put_contents($filePath, $json_album);
+}
 ?>
