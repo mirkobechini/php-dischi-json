aa3026688140caf1436df2f159f2230c0f15960e
mirkobechini <mirkobechini@gmail.com>
2025-12-17 09:55:56 +0100

    Refactor Header component for improved navigation and update album.json formatting

--- components/Header.php
+++ components/Header.php
@@ -1,8 +1,8 @@
 <header>
     <nav class="navbar bg-dark p-1">
         <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="./assets/Spotify_icon.png" alt="Spotify" width="24">
+            <a class="navbar-brand text-light" href="../index.php">
+                <img src="./assets/Spotify_icon.png" alt="Spotify" width="24">&nbsp;Disc
             </a>
         </div>
     </nav>
